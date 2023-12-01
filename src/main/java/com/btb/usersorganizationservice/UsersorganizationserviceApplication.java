d4fd8aa9257a41e51fdbf5b3f6a42d882b6f7e43
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-12-01 17:19:51 +0100

    Spring initializr

---
+++ src/main/java/com/btb/usersorganizationservice/UsersorganizationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.btb.usersorganizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersorganizationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersorganizationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btb/usersorganizationservice/UsersorganizationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.usersorganizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersorganizationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
